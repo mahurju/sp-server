3b8688ef9a9faa1dbe5b00247ae558bb33b28030
MA SEUNG JUN <>
2019-05-14 18:55:51 +0900

    add

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from "typeorm";
 
 const connectionOptions: ConnectionOptions = {
   type: "postgres",
-  database: "maseungjun",
+  database: process.env.DB_NAME || "maseungjun",
   synchronize: true,
   logging: true,
   entities: ["entities/**/*.*"],
